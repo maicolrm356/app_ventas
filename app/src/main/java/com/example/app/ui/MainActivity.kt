21e8c8200d71396f10a38a1a77de9b0f24791825
auxanalista <auxanalista@consuerte.com.co>
2026-05-09 12:08:35 -0500

    segunda clase, inicio de pagina de inicio/login

--- app/src/main/java/com/example/app/ui/MainActivity.kt
+++ app/src/main/java/com/example/app/ui/MainActivity.kt
@@ -1,16 +1,17 @@
-package com.example.app
+package com.example.app.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app.R
 
-class SplashActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
